d56ae6b9ea8a316e79cf7e1da941b094cf25a63b
Elizabeth Banalagay <zbanalagay@gmail.com>
2015-12-15 09:30:13 -0800

    added a button

--- jquery/script.js
+++ jquery/script.js
@@ -1,21 +1,26 @@
 var vidWidth = '800px';
 var vidHeight= '480px';
 $(document).ready(function(){
-  $.get(
-    "https://www.googleapis.com/youtube/v3/search", {
-      part: 'snippet',
-      maxResults: 3,
-      key: "AIzaSyA8iIbu-hvnW6Rb21SAexTtH8Qgbwhzpco"},
-      function(data){
-        var output;
-        $.each(data.items, function(i, item){
-          console.log(item);
-          var videoTitle= item.snippet.title;
-          var videoId= item.id.videoId
+  $("#button").click( function()
+          {
+            $.get(
+              "https://www.googleapis.com/youtube/v3/search", {
+                part: 'snippet',
+                maxResults: 50,
+                key: "AIzaSyA8iIbu-hvnW6Rb21SAexTtH8Qgbwhzpco"},
+                function(data){
+                  var output;
+                  $.each(data.items, function(i, item){
+                    console.log(item);
+                    var videoTitle= item.snippet.title;
+                    var videoId= item.id.videoId
 
-          output= '<div><iframe height="'+vidHeight+'" width="'+ vidWidth + '"src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div>';
-          $('#results').append(output)
-        })
-      }
-  )
+                    output= '<div><iframe height="'+vidHeight+'" width="'+ vidWidth + '"src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></div>';
+                    $('#results').append(output)
+                  })
+                }
+            )
+          }
+     );
+
 })
